49aba53e7cd50fb0e4c90975de2ab9c1abc1eb68
Paul H Trott <paultrott78@gmail.com>
2013-06-20 11:12:02 -0500

    Added the FoodGroupViewController page as a interface page (home) for navigating to food items.

---
+++ FoodApp/FoodGroupsViewController.h
@@ -0,0 +1,27 @@
+//
+//  FoodGroupsViewController.h
+//  FoodApp
+//
+//  Created by Paul Trott on 6/20/13.
+//  Copyright (c) 2013 Paul Trott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FoodViewController.h"
+#import "AboutViewController.h"
+
+@interface FoodGroupsViewController : UIViewController
+{
+    FoodViewController *_foodViewController;
+    AboutViewController *_aboutViewController;
+}
+
+
+- (IBAction)fruitButtonClick:(id)sender;
+- (IBAction)aboutButtonClick:(id)sender;
+
+
+
+
+
+@end
---
+++ FoodApp/FoodGroupsViewController.m
@@ -0,0 +1,76 @@
+//
+//  FoodGroupsViewController.m
+//  FoodApp
+//
+//  Created by Paul Trott on 6/20/13.
+//  Copyright (c) 2013 Paul Trott. All rights reserved.
+//
+
+#import "FoodGroupsViewController.h"
+
+@interface FoodGroupsViewController ()
+
+@end
+
+@implementation FoodGroupsViewController
+
+- (void)dealloc
+{
+    [_foodViewController release]; _foodViewController = nil;
+    [_aboutViewController release]; _aboutViewController = nil;
+    [super dealloc];
+}
+
+- (IBAction)fruitButtonClick:(id)sender
+{
+        //--Add the view of the Food View controller to the current view
+    if (_foodViewController == nil) {
+        _foodViewController = [[FoodViewController alloc] initWithNibName:@"FoodViewController" bundle:nil];
+    }
+    
+        //-- Animate the view.
+    [UIView transitionWithView:self.view
+                      duration:0.5
+                       options:UIViewAnimationOptionTransitionCurlUp | UIViewAnimationOptionLayoutSubviews | UIViewAnimationOptionAllowAnimatedContent
+                    animations:^{[self.view addSubview:_foodViewController.view];}
+                    completion:NULL];
+    
+}
+
+- (IBAction)aboutButtonClick:(id)sender
+{
+        //-Add thee view of the About View Controller to the current view
+    if (_aboutViewController == nil) {
+        _aboutViewController = [[AboutViewController alloc] initWithNibName:@"AboutViewController" bundle:nil];
+    }
+    
+        //--Animate the view.
+    [UIView transitionWithView:self.view
+                      duration:0.5
+                       options:UIViewAnimationOptionTransitionCurlUp | UIViewAnimationOptionLayoutSubviews | UIViewAnimationOptionAllowAnimatedContent
+                    animations:^{[self.view addSubview:_aboutViewController.view];}
+                    completion:NULL];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
