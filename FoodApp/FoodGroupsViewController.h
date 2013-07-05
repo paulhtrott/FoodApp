e3123789899787bb324def0c5ee9f2c21b694f80
Paul H Trott <paultrott78@gmail.com>
2013-07-05 15:57:44 -0500

    Added info for click event for VegetableViewController

--- FoodApp/FoodGroupsViewController.h
+++ FoodApp/FoodGroupsViewController.h
@@ -9,18 +9,23 @@
 #import <UIKit/UIKit.h>
 
 @class FruitViewController;
+@class VegetableViewController;
 @class AboutViewController;
+
 
 @interface FoodGroupsViewController : UIViewController
 {
     FruitViewController *_fruitViewController;
+    VegetableViewController *_vegetableViewController;
     AboutViewController *_aboutViewController;
 }
 
 @property(strong)FruitViewController *fruitViewController;
+@property(strong)VegetableViewController *vegetableViewController;
 @property(strong)AboutViewController *aboutViewController;
 
 - (IBAction)fruitButtonClick:(id)sender;
+- (IBAction)vegetableButtonClick:(id)sender;
 - (IBAction)aboutButtonClick:(id)sender;
 
 
