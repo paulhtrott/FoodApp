c114d46489abd2a30d21a9842a7c4d817939972d
Paul H Trott <paultrott78@gmail.com>
2013-07-05 14:22:27 -0500

    Added all labels and text views needed to display all information on UI

--- FoodApp/DetailViewController.h
+++ FoodApp/DetailViewController.h
@@ -14,6 +14,18 @@
     IBOutlet UILabel *_foodNameLabel;
     IBOutlet UILabel *_foodGroupLabel;
     IBOutlet UILabel *_foodTypeLabel;
+    IBOutlet UILabel *_foodServingSizeLabel;
+    IBOutlet UILabel *_foodCaloriesLabel;
+    IBOutlet UILabel *_foodCaloriesFromFatLabel;
+    IBOutlet UILabel *_foodCarbsLabel;
+    IBOutlet UILabel *_foodProteinLabel;
+    IBOutlet UILabel *_foodFatLabel;
+    
+    IBOutlet UITextView *_foodDescription;
+    IBOutlet UITextView *_foodHealthBenefits;
+    IBOutlet UITextView *_foodHealthRisks;
+    
+    IBOutlet UIScrollView *_scrollView;
     
     Food *_detailFood;
 }
@@ -21,6 +33,18 @@
 @property (strong, nonatomic) UILabel *foodNameLabel;
 @property (strong, nonatomic) UILabel *foodGroupLabel;
 @property (strong, nonatomic) UILabel *foodTypeLabel;
+@property (strong, nonatomic) UILabel *foodServingSizeLabel;
+@property (strong, nonatomic) UILabel *foodCaloriesLabel;
+@property (strong, nonatomic) UILabel *foodCaloriesFromFatLabel;
+@property (strong, nonatomic) UILabel *foodCarbsLabel;
+@property (strong, nonatomic) UILabel *foodProteinLabel;
+@property (strong, nonatomic) UILabel *foodFatLabel;
+
+@property (strong, nonatomic) UITextView *foodDescription;
+@property (strong, nonatomic) UITextView *foodHealthBenefits;
+@property (strong, nonatomic) UITextView *foodHealthRisks;
+
+@property (strong, nonatomic) UIScrollView *scrollView;
 
 @property (strong, nonatomic) Food *detailFood;
 
