5e2107c8aa0ff9f61b9d784e6483c01225dbb29f
Paul H Trott <paultrott78@gmail.com>
2013-07-26 16:42:57 -0500

    Added sqlite3 as a cocoapod.
    Added Podfile to project. Installed sqlite3 as a pod, which added
    workspace.

---
+++ Podfile
@@ -0,0 +1,3 @@
+#------ Pods for Food app -----
+#--- sqlite3 pod for database queries ---
+pod 'sqlite3', '~> 3.7.17.0'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// sqlite3
+#define COCOAPODS_POD_AVAILABLE_sqlite3
+#define COCOAPODS_VERSION_MAJOR_sqlite3 3
+#define COCOAPODS_VERSION_MINOR_sqlite3 7
+#define COCOAPODS_VERSION_PATCH_sqlite3 17
+
