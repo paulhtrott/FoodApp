f445017735b6ff3c2fecd4d49420a9bea06b8b7f
Paul H Trott <paultrott78@gmail.com>
2013-08-02 09:24:17 -0500

    Added JSONKit pod for JSON

--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 #------ Pods for Food app -----
 #--- sqlite3 pod for database queries ---
 pod 'sqlite3', '~> 3.7.17.0'
-#--- AFNetworking for web requests ---
+#--- AFNetworking for web requests with JSON kit ---
 pod 'AFNetworking'
+pod 'JSONKit'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,13 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 3
 #define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
+// JSONKit
+#define COCOAPODS_POD_AVAILABLE_JSONKit
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.5pre.
+
 // sqlite3
 #define COCOAPODS_POD_AVAILABLE_sqlite3
 #define COCOAPODS_VERSION_MAJOR_sqlite3 3
